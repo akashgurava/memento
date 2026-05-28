40d073443bcc4bccf2ccaa3ccdced9528a6e9f28
Akash Gurava <akashgurava@outlook.com>
2026-05-28 16:52:27 +0530

    create cli

--- memento/src/lib.rs
+++ memento/src/lib.rs
@@ -1,3 +1,8 @@
+mod config;
 mod error;
 
+pub use crate::config::AppConfig;
 pub use crate::error::MementoError;
+
+#[cfg(feature = "cli")]
+pub use crate::config::Cli;
--- memento/src/main.rs
+++ memento/src/main.rs
@@ -1,1 +1,9 @@
+use clap::Parser;
 
+use memento::{AppConfig, Cli, MementoError};
+
+fn main() -> Result<(), MementoError> {
+    let config = AppConfig::from_cli(Cli::parse())?;
+    println!("{config:#?}");
+    Ok(())
+}
